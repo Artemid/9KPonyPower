cbc65cac8dcfff54bec9e8bab4a7ce36b3ac9151
Mikhail Udalyi <mikhail_udalyi@epam.com>
2016-06-01 17:42:30 +0300

    I added a reset method. But somebody will need to bind it to a key of joystick.

---
+++ UnityProject/Assets/Scripts/ReverterManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ReverterManager : MonoBehaviour
+{
+    static int indexOfCurScene = -1;
+
+    public static void ResetScene()
+    {
+        indexOfCurScene = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene("Reverter");
+    }
+
+    protected void Start()
+    {
+        if (indexOfCurScene != -1)
+        {
+            StartCoroutine(Load());
+        }
+    }
+
+    IEnumerator Load()
+    {
+
+        yield return new WaitForSeconds(3);
+
+        SceneManager.LoadScene(indexOfCurScene);
+    }
+}
